d4222533550efdcec8c40b0fd373b091dae7ec1f
Thomas Angelbo Christensen <roosteronacid@gmail.com>
2014-08-25 20:21:10 +0200

    I don't even..

--- js/index.js
+++ js/index.js
@@ -1,4 +1,66 @@
-(function ()
+var Ratings = function (collection)
 {
-	return;
-}());
+    var ratings = [];
+    var stars = [];
+
+
+    (function () /* populate ratings array */
+    {
+		var index = collection.length;
+
+		while (index)
+		{
+			index--;
+
+			ratings.push(parseInt(collection[index].starRating, 10));
+		}
+    }());
+
+
+    this.getCollection = function ()
+    {
+    	return collection;
+    };
+
+
+    this.getRatings = function ()
+    {
+    	return ratings;
+    };
+
+
+    this.getAverageRating = function ()
+    {
+    	var length = ratings.length;
+    	var index = length;
+    	var sum = 0;
+
+    	while (index)
+		{
+			index--;
+
+			sum += ratings[index];
+		}
+
+		return Math.ceil(sum / (length - 1));
+    };
+};
+
+
+$(function ()
+{
+	$.ajax(
+	{
+		url: "resources/reviews.json",
+
+		success: function (data)
+		{
+			window.ratings = new Ratings(data.reviews);
+		},
+
+		error: function ()
+		{
+			console.log("error", arguments);
+		},
+	})
+});
